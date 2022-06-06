d5778c4b11956614a7dffc512f37586a855764b2
iminghsu <iminghsu@cw.com.tw>
2022-06-06 16:10:49 +0800

    feat: first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var static = require('node-static');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}
+const file = new (static.Server)();
+
+https.createServer(options, function (req, res) {
+    req.addListener('end', function () {
+        file.serve(req, res);
+    }).resume();
+}).listen(3000);
+
+
+http.createServer(options, function (req, res) {
+    req.addListener('end', function () {
+        file.serve(req, res);
+    }).resume();
+}).listen(3001);
